540712c4b90cecd36610a2e051ab68915db9f5e4
Gert Dingenen <gert.dingenen@appfoundry.be>
2019-12-11 15:56:10 +0100

    Replace Retrofit's deprecated Deferred CallAdapter with its built-in suspend support.

--- app/src/main/java/com/dropbox/android/sample/data/remote/Api.kt
+++ app/src/main/java/com/dropbox/android/sample/data/remote/Api.kt
@@ -1,7 +1,6 @@
 package com.dropbox.android.sample.data.remote
 
 import com.dropbox.android.sample.data.model.RedditData
-import kotlinx.coroutines.Deferred
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -10,14 +9,14 @@ import retrofit2.http.Query
 interface Api {
 
     @GET("r/{subredditName}/new/.json")
-    fun fetchSubreddit(
+    suspend fun fetchSubreddit(
         @Path("subredditName") subredditName: String,
         @Query("limit") limit: String
-    ): Deferred<RedditData>
+    ): RedditData
 
     @GET("r/{subredditName}/new/.json")
-    fun fetchSubredditForPersister(
+    suspend fun fetchSubredditForPersister(
         @Path("subredditName") subredditName: String,
         @Query("limit") limit: String
-    ): Deferred<ResponseBody>
+    ): ResponseBody
 }
