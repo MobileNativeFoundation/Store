594fbcbb104ca5c679c0cd0d7c69d52f6e53b1dc
Tournaris Pavlos-Petros <p.tournaris@gmail.com>
2019-12-17 23:09:05 +0200

    Convert BarCodePathResolver to Object instead of Class

--- filesystem/src/main/java/com/dropbox/android/external/fs3/BarCodePathResolver.kt
+++ filesystem/src/main/java/com/dropbox/android/external/fs3/BarCodePathResolver.kt
@@ -2,6 +2,6 @@ package com.dropbox.android.external.fs3
 
 import com.dropbox.android.external.store4.legacy.BarCode
 
-class BarCodePathResolver : PathResolver<BarCode> {
+object BarCodePathResolver : PathResolver<BarCode> {
     override fun resolve(key: BarCode): String = key.toString()
 }
