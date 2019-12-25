9ce4941664daff94935c1c3073bba4fe134c8926
Fabio Collini <fabio.collini@gmail.com>
2019-12-25 16:30:01 +0100

    New sample with a ViewModel and multiple stores (#44)
    * New sample with a ViewModel and multiple stores
    
    * Fix warnings

--- app/src/main/java/com/dropbox/android/sample/SampleApp.kt
+++ app/src/main/java/com/dropbox/android/sample/SampleApp.kt
@@ -5,15 +5,19 @@ import com.dropbox.android.external.store4.Persister
 import com.dropbox.android.external.store4.Store
 import com.dropbox.android.external.store4.legacy.BarCode
 import com.dropbox.android.sample.data.model.Post
-import java.io.IOException
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import okio.BufferedSource
+import java.io.IOException
 
 @FlowPreview
 @ExperimentalCoroutinesApi
 class SampleApp : Application() {
     lateinit var roomStore: Store<String, List<Post>>
+
+    lateinit var storeMultiParam: Store<Pair<String, RedditConfig>, List<Post>>
+
+    lateinit var configStore: Store<Unit, RedditConfig>
 
     lateinit var persister: Persister<BufferedSource, BarCode>
 
@@ -23,6 +27,8 @@ class SampleApp : Application() {
         super.onCreate()
         initPersister()
         roomStore = Graph.provideRoomStore(this)
+        storeMultiParam = Graph.provideRoomStoreMultiParam(this)
+        configStore = Graph.provideConfigStore(this)
     }
 
     private fun initPersister() {
