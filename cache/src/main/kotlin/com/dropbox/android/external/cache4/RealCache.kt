3028df68c2c9ff7870a27b7fa1a0f9232aaf4e10
Yang Chen <ychescale9@gmail.com>
2020-01-01 14:04:45 +1100

    Make CacheEntry's Key and Value bound to Any, move CacheEntry to RealCache.kt as a private class.

--- cache/src/main/kotlin/com/dropbox/android/external/cache4/RealCache.kt
+++ cache/src/main/kotlin/com/dropbox/android/external/cache4/RealCache.kt
@@ -267,3 +267,19 @@ internal class RealCache<Key : Any, Value : Any>(
         private const val CONCURRENCY_LEVEL = 4
     }
 }
+
+/**
+ * A cache entry holds the [key] and [value] pair,
+ * along with the metadata needed to perform cache expiration and eviction.
+ *
+ * A cache entry can be reused by updating [value], [accessTimeNanos], or [writeTimeNanos],
+ * as this allows us to avoid creating new instance of [CacheEntry] on every access and write.
+ *
+ * Note that we assume cache entries are stored in thread-safe collections such as ConcurrentHashMap.
+ */
+private data class CacheEntry<Key : Any, Value : Any>(
+    val key: Key,
+    var value: Value,
+    var accessTimeNanos: Long = Long.MAX_VALUE,
+    var writeTimeNanos: Long = Long.MAX_VALUE
+)
