e252460f3b6d4f4aa814a5def7d1bf591283d310
Yang Chen <ychescale9@gmail.com>
2020-01-12 00:06:50 +1100

    Fix compiler warnings.

--- app/src/main/java/com/dropbox/android/sample/RedditViewModel.kt
+++ app/src/main/java/com/dropbox/android/sample/RedditViewModel.kt
@@ -8,8 +8,11 @@ import com.dropbox.android.external.store4.fresh
 import com.dropbox.android.external.store4.get
 import com.dropbox.android.sample.data.model.Post
 import com.dropbox.android.sample.utils.Lce
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.launch
 
+@UseExperimental(FlowPreview::class, ExperimentalCoroutinesApi::class)
 class RedditViewModel(
     app: Application
 ) : AndroidViewModel(app) {
