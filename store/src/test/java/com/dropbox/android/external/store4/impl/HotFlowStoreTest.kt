2676e06f70ef1743f6cdf0695c0be80b9631afcf
miken <miken@dropbox.com>
2020-02-12 20:00:04 -0500

    lint

--- store/src/test/java/com/dropbox/android/external/store4/impl/HotFlowStoreTest.kt
+++ store/src/test/java/com/dropbox/android/external/store4/impl/HotFlowStoreTest.kt
@@ -4,17 +4,17 @@ import com.dropbox.android.external.store4.ResponseOrigin
 import com.dropbox.android.external.store4.StoreBuilder
 import com.dropbox.android.external.store4.StoreRequest
 import com.dropbox.android.external.store4.StoreResponse
+import com.dropbox.android.external.store4.testutil.assertThat
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import com.dropbox.android.external.store4.testutil.*
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 
 @ExperimentalCoroutinesApi
 @FlowPreview
@@ -22,44 +22,45 @@ import kotlinx.coroutines.flow.flowOf
 class HotFlowStoreTest {
     private val testScope = TestCoroutineScope()
     @Test
-    fun `GIVEN a hot fetcher WHEN two cached and one fresh call THEN fetcher is only called twice`() = testScope.runBlockingTest {
-        val fetcher = FakeFlowFetcher(
-            3 to "three-1",
-            3 to "three-2"
-        )
-        val pipeline = StoreBuilder.from <Int, String>{ fetcher.fetch(it) }
-            .scope(testScope)
-            .build()
+    fun `GIVEN a hot fetcher WHEN two cached and one fresh call THEN fetcher is only called twice`() =
+        testScope.runBlockingTest {
+            val fetcher = FakeFlowFetcher(
+                3 to "three-1",
+                3 to "three-2"
+            )
+            val pipeline = StoreBuilder.from<Int, String> { fetcher.fetch(it) }
+                .scope(testScope)
+                .build()
 
-        assertThat(pipeline.stream(StoreRequest.cached(3, refresh = false)))
-            .emitsExactly(
-                StoreResponse.Loading(
-                    origin = ResponseOrigin.Fetcher
-                ), StoreResponse.Data(
-                    value = "three-1",
-                    origin = ResponseOrigin.Fetcher
+            assertThat(pipeline.stream(StoreRequest.cached(3, refresh = false)))
+                .emitsExactly(
+                    StoreResponse.Loading(
+                        origin = ResponseOrigin.Fetcher
+                    ), StoreResponse.Data(
+                        value = "three-1",
+                        origin = ResponseOrigin.Fetcher
+                    )
                 )
-            )
-        assertThat(
-            pipeline.stream(StoreRequest.cached(3, refresh = false))
-        ).emitsExactly(
-            StoreResponse.Data(
-                value = "three-1",
-                origin = ResponseOrigin.Cache
-            )
-        )
-
-        assertThat(pipeline.stream(StoreRequest.fresh(3)))
-            .emitsExactly(
-                StoreResponse.Loading(
-                    origin = ResponseOrigin.Fetcher
-                ),
+            assertThat(
+                pipeline.stream(StoreRequest.cached(3, refresh = false))
+            ).emitsExactly(
                 StoreResponse.Data(
-                    value = "three-2",
-                    origin = ResponseOrigin.Fetcher
+                    value = "three-1",
+                    origin = ResponseOrigin.Cache
                 )
             )
-    }
+
+            assertThat(pipeline.stream(StoreRequest.fresh(3)))
+                .emitsExactly(
+                    StoreResponse.Loading(
+                        origin = ResponseOrigin.Fetcher
+                    ),
+                    StoreResponse.Data(
+                        value = "three-2",
+                        origin = ResponseOrigin.Fetcher
+                    )
+                )
+        }
 }
 
 class FakeFlowFetcher<Key, Output>(
