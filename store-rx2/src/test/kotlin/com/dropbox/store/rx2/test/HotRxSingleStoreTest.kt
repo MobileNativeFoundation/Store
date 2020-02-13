7b7f01a439fb4d013d3129c3c2df2143ec78a507
Mike Nakhimovich <digitalbuddha@users.noreply.github.com>
2020-02-12 20:36:10 -0500

    Rxbindings (#93)
    RxJava2 extensions

---
+++ store-rx2/src/test/kotlin/com/dropbox/store/rx2/test/HotRxSingleStoreTest.kt
@@ -0,0 +1,75 @@
+package com.dropbox.store.rx2.test
+
+import com.dropbox.android.external.store4.ResponseOrigin
+import com.dropbox.android.external.store4.StoreBuilder
+import com.dropbox.android.external.store4.StoreRequest
+import com.dropbox.android.external.store4.StoreResponse
+import com.dropbox.store.rx2.fromSingle
+import com.google.common.truth.Truth.assertThat
+import io.reactivex.Single
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class HotRxSingleStoreTest {
+    private val testScope = TestCoroutineScope()
+    @Test
+    fun `GIVEN a hot fetcher WHEN two cached and one fresh call THEN fetcher is only called twice`() =
+        testScope.runBlockingTest {
+            val fetcher = FakeFetcher(
+                3 to "three-1",
+                3 to "three-2"
+            )
+            val pipeline = StoreBuilder.fromSingle<Int, String> { fetcher.fetch(it) }
+                .scope(testScope)
+                .build()
+
+            assertThat(pipeline.stream(StoreRequest.cached(3, refresh = false)))
+                .emitsExactly(
+                    StoreResponse.Loading<String>(
+                        origin = ResponseOrigin.Fetcher
+                    ), StoreResponse.Data(
+                        value = "three-1",
+                        origin = ResponseOrigin.Fetcher
+                    )
+                )
+            assertThat(
+                pipeline.stream(StoreRequest.cached(3, refresh = false))
+            ).emitsExactly(
+                StoreResponse.Data(
+                    value = "three-1",
+                    origin = ResponseOrigin.Cache
+                )
+            )
+
+            assertThat(pipeline.stream(StoreRequest.fresh(3)))
+                .emitsExactly(
+                    StoreResponse.Loading<String>(
+                        origin = ResponseOrigin.Fetcher
+                    ),
+                    StoreResponse.Data(
+                        value = "three-2",
+                        origin = ResponseOrigin.Fetcher
+                    )
+                )
+        }
+}
+
+class FakeFetcher<Key, Output>(
+    vararg val responses: Pair<Key, Output>
+) {
+    private var index = 0
+    @Suppress("RedundantSuspendModifier") // needed for function reference
+    fun fetch(key: Key): Single<Output> {
+        // will throw if fetcher called more than twice
+        if (index >= responses.size) {
+            throw AssertionError("unexpected fetch request")
+        }
+        val pair = responses[index++]
+        assertThat(pair.first).isEqualTo(key)
+        return Single.just(pair.second)
+    }
+}
---
+++ store-rx2/src/test/kotlin/com/dropbox/store/rx2/test/RxFlowableStoreTest.kt
@@ -0,0 +1,79 @@
+package com.dropbox.store.rx2.test
+
+import com.dropbox.android.external.store4.ResponseOrigin
+import com.dropbox.android.external.store4.StoreBuilder
+import com.dropbox.android.external.store4.StoreRequest
+import com.dropbox.android.external.store4.StoreResponse
+import com.dropbox.store.rx2.observe
+import com.dropbox.store.rx2.fromFlowable
+import com.dropbox.store.rx2.withFlowablePersister
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.Single
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import java.util.concurrent.atomic.AtomicInteger
+
+@RunWith(JUnit4::class)
+class RxFlowableStoreTest {
+    val atomicInteger = AtomicInteger(0)
+    val fakeDisk = mutableMapOf<Int, String>()
+    val store =
+        StoreBuilder.fromFlowable<Int, String> {
+            Flowable.create({ emitter ->
+                emitter.onNext("$it ${atomicInteger.incrementAndGet()} occurrence")
+                emitter.onNext("$it ${atomicInteger.incrementAndGet()} occurrence")
+                emitter.onComplete()
+            }, BackpressureStrategy.LATEST)
+        }
+            .withFlowablePersister(
+                reader = {
+                    if (fakeDisk[it] != null)
+                        Flowable.fromCallable { fakeDisk[it]!! }
+                    else
+                        Flowable.empty()
+                },
+                writer = { key, value ->
+                    Single.fromCallable { fakeDisk[key] = value }
+                }
+            )
+            .build()
+
+    @Test
+    fun simpleTest() {
+        val values = store.observe(StoreRequest.fresh(3))
+            .test()
+            .awaitCount(3)
+            .assertValues(
+                StoreResponse.Loading<String>(ResponseOrigin.Fetcher),
+                StoreResponse.Data("3 1 occurrence", ResponseOrigin.Fetcher),
+                StoreResponse.Data("3 2 occurrence", ResponseOrigin.Fetcher)
+            )
+
+        val values2 = store.observe(StoreRequest.cached(3, false))
+            .test()
+            .awaitCount(2)
+            .assertValues(
+                StoreResponse.Data("3 2 occurrence", ResponseOrigin.Cache),
+                StoreResponse.Data("3 2 occurrence", ResponseOrigin.Persister)
+            )
+
+        val values3 = store.observe(StoreRequest.fresh(3))
+            .test()
+            .awaitCount(3)
+            .assertValues(
+                StoreResponse.Loading<String>(ResponseOrigin.Fetcher),
+                StoreResponse.Data("3 3 occurrence", ResponseOrigin.Fetcher),
+                StoreResponse.Data("3 4 occurrence", ResponseOrigin.Fetcher)
+            )
+
+        val values4 = store.observe(StoreRequest.cached(3, false))
+            .test()
+            .awaitCount(2)
+            .assertValues(
+                StoreResponse.Data("3 4 occurrence", ResponseOrigin.Cache),
+                StoreResponse.Data("3 4 occurrence", ResponseOrigin.Persister)
+            )
+    }
+}
