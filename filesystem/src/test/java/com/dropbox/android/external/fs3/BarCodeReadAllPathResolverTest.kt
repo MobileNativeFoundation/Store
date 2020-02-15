9f04a263d1db8f6a4e214554cad907713ce3e084
miken <miken@dropbox.com>
2020-02-15 13:46:47 -0500

    ktlint fix

--- filesystem/src/test/java/com/dropbox/android/external/fs3/BarCodeReadAllPathResolverTest.kt
+++ filesystem/src/test/java/com/dropbox/android/external/fs3/BarCodeReadAllPathResolverTest.kt
@@ -1,16 +1,14 @@
 package com.dropbox.android.external.fs3
 
 import com.dropbox.android.external.store4.legacy.BarCode
-import org.junit.Test
-
-import org.junit.Assert.*
 import com.google.common.truth.Truth.assertThat
+import org.junit.Test
 
 class BarCodeReadAllPathResolverTest {
 
     @Test
     fun resolve() {
-       assertThat (BarCodeReadAllPathResolver.resolve(BarCode("a", "b")))
-           .isEqualTo("a/b")
+        assertThat(BarCodeReadAllPathResolver.resolve(BarCode("a", "b")))
+            .isEqualTo("a/b")
     }
 }
