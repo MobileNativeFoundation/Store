9111d667c52c50a438491b27b2321d5fd5c1684c
Mike Nakhimovich <digitalbuddha@users.noreply.github.com>
2020-02-16 08:35:35 -0500

    more tests! (#106)
    * more tests!
    
    * Start tracking API binary compatibility (#97)
    
    * Configure binary-compatibility-validator plugin.
    
    * Generate current API dumps.
    
    * Update CONTRUBUTON.md with instruction for updating API dumps.
    
    * ktlint fix
    
    * newlines
    
    * put fs eraser back in
    
    * more tests!
    
    * ktlint fix
    
    * newlines
    
    * put fs eraser back in
    
    * run api checks
    
    * lint
    
    * lint
    
    Co-authored-by: Yang <ychescale9@gmail.com>

---
+++ store/src/test/java/com/dropbox/android/external/store4/impl/operators/MapIndexedKtTest.kt
@@ -0,0 +1,16 @@
+package com.dropbox.android.external.store4.impl.operators
+
+import com.dropbox.android.external.store4.testutil.assertThat
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+
+class MapIndexedKtTest {
+    val scope = TestCoroutineScope()
+    @Test
+    fun mapIndexed() = runBlockingTest {
+        assertThat(flowOf(5, 6).mapIndexed { index, value -> index to value })
+            .emitsExactly(0 to 5, 1 to 6)
+    }
+}
