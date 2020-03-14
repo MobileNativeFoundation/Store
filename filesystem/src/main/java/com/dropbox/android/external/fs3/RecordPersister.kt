ebabb388cdded48707995b48742e17f9c2af7abc
Claus Holst <clhols@gmail.com>
2020-03-14 10:45:19 +0100

    Remove usage of javax.inject

--- filesystem/src/main/java/com/dropbox/android/external/fs3/RecordPersister.kt
+++ filesystem/src/main/java/com/dropbox/android/external/fs3/RecordPersister.kt
@@ -2,13 +2,11 @@ package com.dropbox.android.external.fs3
 
 import com.dropbox.android.external.fs3.filesystem.FileSystem
 import com.dropbox.android.external.store4.legacy.BarCode
-import javax.inject.Inject
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
-class RecordPersister @Inject
-constructor(
+class RecordPersister(
     fileSystem: FileSystem,
     private val expirationDuration: Duration
 ) : SourcePersister(fileSystem), RecordProvider<BarCode> {
