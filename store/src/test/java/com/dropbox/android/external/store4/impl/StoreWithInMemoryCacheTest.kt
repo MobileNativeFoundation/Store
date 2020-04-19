e6158669d46a3b4255eacea4a9bdb74e74803146
Yang <ychescale9@gmail.com>
2020-04-20 09:50:12 +1000

    Fix a deadlock during cache eviction (#155)
    * Fix a deadlock during cache eviction due to duplicate CacheEntries with different accessTime being added to accessOrder HashSet.
    
    * Rename test.

---
+++ store/src/test/java/com/dropbox/android/external/store4/impl/StoreWithInMemoryCacheTest.kt
@@ -0,0 +1,42 @@
+package com.dropbox.android.external.store4.impl
+
+import com.dropbox.android.external.store4.MemoryPolicy
+import com.dropbox.android.external.store4.StoreBuilder
+import com.dropbox.android.external.store4.get
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import kotlin.time.ExperimentalTime
+import kotlin.time.minutes
+
+@FlowPreview
+@ExperimentalTime
+@ExperimentalStdlibApi
+@ExperimentalCoroutinesApi
+@RunWith(JUnit4::class)
+class StoreWithInMemoryCacheTest {
+
+    @Test
+    fun `store requests can complete when its in-memory cache (with access expiry) is at the maximum size`() {
+        val store = StoreBuilder
+            .fromNonFlow { _: Int -> "result" }
+            .cachePolicy(
+                MemoryPolicy
+                    .builder()
+                    .setExpireAfterAccess(10.minutes)
+                    .setMemorySize(1)
+                    .build()
+            )
+            .build()
+
+        runBlocking {
+            store.get(0)
+            store.get(0)
+            store.get(1)
+            store.get(2)
+        }
+    }
+}
