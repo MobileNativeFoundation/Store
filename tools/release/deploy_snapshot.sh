6bcf1151f0b1bf71e79f230caa69959682c996ce
David Chang <changd@gmail.com>
2020-08-20 07:59:22 -0700

    Use absolute path for signing key (#214)

--- tools/release/deploy_snapshot.sh
+++ tools/release/deploy_snapshot.sh
@@ -23,6 +23,6 @@ else
   echo "Deploying store..."
   openssl aes-256-cbc -md sha256 -d -in tools/release/secring.gpg.aes -out tools/release/secring.gpg -k "${ENCRYPT_KEY}"
   # https://docs.gradle.org/current/userguide/signing_plugin.html#sec:signatory_credentials
-  ./gradlew uploadArchives -PSONATYPE_USERNAME="${SONATYPE_USERNAME}" -PSONATYPE_PASSWORD="${SONATYPE_PASSWORD}" -Psigning.keyId="${SIGNING_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile=tools/release/secring.gpg
+  ./gradlew uploadArchives -PSONATYPE_USERNAME="${SONATYPE_USERNAME}" -PSONATYPE_PASSWORD="${SONATYPE_PASSWORD}" -Psigning.keyId="${SIGNING_ID}" -Psigning.password="${SIGNING_PASSWORD}" -Psigning.secretKeyRingFile=${PWD}/tools/release/secring.gpg
   echo "Store deployed!"
 fi
