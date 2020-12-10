f8136c8c0377d577995acbc3f7977a051a7028b2
James Karsh <jkarsh@idelic.com>
2020-12-10 13:41:18 -0500

    Add Jetbrains copyrights back to coroutine testing classes

--- kmp/src/commonTest/kotlin/com/dropbox/kmp/external/store4/testutil/coroutines/DelayController.kt
+++ kmp/src/commonTest/kotlin/com/dropbox/kmp/external/store4/testutil/coroutines/DelayController.kt
@@ -1,8 +1,8 @@
-package com.dropbox.kmp.external.store4.testutil.coroutines
-
 /*
  * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
+
+package com.dropbox.kmp.external.store4.testutil.coroutines
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
