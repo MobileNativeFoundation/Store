0945d9f214c11f6037fe8fd722323090d54e0be7
miken <miken@dropbox.com>
2021-11-16 14:00:52 -0500

    add rx3 comment as well

--- store-rx2/src/main/kotlin/com/dropbox/store/rx2/RxStoreBuilder.kt
+++ store-rx2/src/main/kotlin/com/dropbox/store/rx2/RxStoreBuilder.kt
@@ -1,5 +1,6 @@
 package com.dropbox.store.rx2
 
+import com.dropbox.android.external.store4.Store
 import com.dropbox.android.external.store4.StoreBuilder
 import io.reactivex.Scheduler
 import kotlinx.coroutines.CoroutineScope
@@ -7,8 +8,16 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.rx2.asCoroutineDispatcher
 
 /**
- * Define what scheduler fetcher requests will be called on,
- * if a scheduler is not set Store will use [GlobalScope]
+ *  A store multicasts same [Output] value to many consumers (Similar to RxJava.share()), by default
+ *  [Store] will open a global scope for management of shared responses, if instead you'd like to control
+ *  the scheduler that sharing/multicasting happens in you can pass a @param [scheduler]
+ *
+ *  Note this does not control what scheduler a response is emitted on but rather what thread/scheduler
+ *  to use when managing in flight responses. This is usually used for things like testing where you
+ *  may want to confine to a scheduler backed by a single thread executor
+ *
+ *   @param scheduler - scheduler to use for sharing
+ *  if a scheduler is not set Store will use [GlobalScope]
  */
 fun <Key : Any, Output : Any> StoreBuilder<Key, Output>.withScheduler(
     scheduler: Scheduler
