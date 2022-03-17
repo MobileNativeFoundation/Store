35a6e90ba6589f62675f2f7bcd3659dbe2c96516
Sam Edwards <sam@handstandtech.com>
2022-03-17 12:30:34 -0400

    Remove Kotlin Android Extensions (#416)
    * Removed Kotlin Android Extensions from Sample App
    
    * Fixed formatting
    
    Co-authored-by: Sam Edwards <samedwards@dropbox.com>

--- app/src/main/java/com/dropbox/android/sample/ui/reddit/RedditFragment.kt
+++ app/src/main/java/com/dropbox/android/sample/ui/reddit/RedditFragment.kt
@@ -4,22 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.dropbox.android.sample.R
 import com.dropbox.android.sample.data.model.Post
 import com.dropbox.android.sample.reddit.PostAdapter
 import com.dropbox.android.sample.utils.Lce
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.fragment_room_store.*
-import kotlinx.android.synthetic.main.activity_store.postRecyclerView
 
 class RedditFragment : Fragment() {
 
     private val viewModel: RedditViewModel by viewModels()
 
     private val adapter = PostAdapter()
+
+    private val pullToRefresh: SwipeRefreshLayout get() = requireView().findViewById(R.id.pullToRefresh)
+
+    private val subredditInput: EditText get() = requireView().findViewById(R.id.subredditInput)
+
+    private val fetchButton: Button get() = requireView().findViewById(R.id.fetchButton)
+
+    private val postRecyclerView: RecyclerView get() = requireView().findViewById(R.id.postRecyclerView)
+
+    private val root: CoordinatorLayout get() = requireView().findViewById(R.id.root)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_room_store, container, false)
