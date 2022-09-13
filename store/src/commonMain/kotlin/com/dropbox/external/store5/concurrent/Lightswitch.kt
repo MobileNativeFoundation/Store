5bbb302ffd77a341c803a1b16749238090f9470d
mramotar <mramotar@dropbox.com>
2022-09-13 18:03:01 -0400

    Document Lightswitch
    Signed-off-by: mramotar <mramotar@dropbox.com>

--- store/src/commonMain/kotlin/com/dropbox/external/store5/concurrent/Lightswitch.kt
+++ store/src/commonMain/kotlin/com/dropbox/external/store5/concurrent/Lightswitch.kt
@@ -2,6 +2,11 @@ package com.dropbox.external.store5.concurrent
 
 import kotlinx.coroutines.sync.Mutex
 
+/**
+ * Locks when first reader starts and unlocks when last reader finishes.
+ * Lightswitch analogy: First one into a room turns on the light (locks the mutex), and the last one out turns off the light (unlocks the mutex).
+ * @property counter Number of readers
+ */
 internal class Lightswitch {
     private var counter = 0
     private val mutex = Mutex()
