238d233d00760ff19677f87d730fea8b944e7d8d
Matt Ramotar <mramotar@dropbox.com>
2022-09-28 14:34:57 -0400

    Add app and main activity
    Signed-off-by: Matt Ramotar <mramotar@dropbox.com>

---
+++ samples/notes/android/app/src/main/kotlin/com/dropbox/external/store5/sample/notes/android/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dropbox.external.store5.sample.notes.android.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { }
+    }
+}
