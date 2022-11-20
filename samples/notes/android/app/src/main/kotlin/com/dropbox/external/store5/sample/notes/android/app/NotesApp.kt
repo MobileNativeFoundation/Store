1e481c887ee7edf6bcf107150219ce1eac8d7394
mramotar <mramotar@dropbox.com>
2022-11-20 14:56:50 -0500

    Fix rebase issue
    Signed-off-by: mramotar <mramotar@dropbox.com>

--- samples/notes/android/app/src/main/kotlin/com/dropbox/external/store5/sample/notes/android/app/NotesApp.kt
+++
@@ -1,5 +0,0 @@
-package com.dropbox.external.store5.sample.notes.android.app
-
-import android.app.Application
-
-class NotesApp : Application()
--- samples/notes/android/app/src/main/kotlin/com/dropbox/external/store5/sample/notes/android/app/wiring/AppComponent.kt
+++
@@ -1,9 +0,0 @@
-package com.dropbox.external.store5.sample.notes.android.app.wiring
-
-import com.dropbox.external.store5.sample.notes.android.common.scoping.AppScope
-import com.dropbox.external.store5.sample.notes.android.common.scoping.SingleIn
-import com.squareup.anvil.annotations.MergeComponent
-
-@SingleIn(AppScope::class)
-@MergeComponent(AppScope::class)
-interface AppComponent
