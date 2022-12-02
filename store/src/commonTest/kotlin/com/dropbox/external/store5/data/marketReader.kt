6442a8acb69845072f359e3a5793dcf5ba5b3be2
mramotar <mramotar@dropbox.com>
2022-12-02 12:29:00 -0500

    Add ItemValidator
    Signed-off-by: mramotar <mramotar@dropbox.com>

--- store/src/commonTest/kotlin/com/dropbox/external/store5/data/marketReader.kt
+++ store/src/commonTest/kotlin/com/dropbox/external/store5/data/marketReader.kt
@@ -1,5 +1,6 @@
 package com.dropbox.external.store5.data
 
+import com.dropbox.external.store5.ItemValidator
 import com.dropbox.external.store5.MarketReader
 import com.dropbox.external.store5.OnMarketCompletion
 import com.dropbox.external.store5.data.model.Note
@@ -12,4 +13,16 @@ internal fun marketReader(
     key = key,
     onCompletions = onCompletions,
     refresh = refresh
+)
+
+internal fun marketReaderWithValidator(
+    key: String,
+    isValid: Boolean = true,
+    refresh: Boolean = false,
+    onCompletions: List<OnMarketCompletion<Note>> = listOf()
+): MarketReader<String, Note, Note> = MarketReader.by(
+    key = key,
+    validator = ItemValidator.by { isValid },
+    refresh = refresh,
+    onCompletions = onCompletions
 )
