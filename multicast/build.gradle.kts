9302b115ce8162aaefec67a729f56d6c33bf7079
Matt Ramotar <mramotar@dropbox.com>
2022-12-16 14:28:00 -0500

    Merge multicast from github.com/MobileNativeFoundation/Store/tree/Kmp
    Signed-off-by: Matt Ramotar <mramotar@dropbox.com>

---
+++ multicast/build.gradle.kts
@@ -0,0 +1,118 @@
+@file:Suppress("UnstableApiUsage")
+
+import com.vanniktech.maven.publish.SonatypeHost.S01
+import org.jetbrains.dokka.gradle.DokkaTask
+
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+    id("com.android.library")
+    id("com.vanniktech.maven.publish")
+    id("org.jetbrains.dokka")
+    id("org.jetbrains.kotlinx.kover")
+    id("co.touchlab.faktory.kmmbridge") version Version.kmmBridge
+    `maven-publish`
+    kotlin("native.cocoapods")
+    id("kotlinx-atomicfu")
+}
+
+kotlin {
+    android()
+    jvm()
+    iosArm64()
+    iosX64()
+    js {
+        browser()
+        nodejs()
+    }
+    cocoapods {
+        summary = "Multicast5"
+        homepage = "https://github.com/MobileNativeFoundation/Store"
+        ios.deploymentTarget = "13"
+        version = Version.store
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(Deps.Kotlinx.coroutinesCore)
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(Deps.Kotlinx.stdLib)
+                implementation(Deps.Kotlinx.coroutinesCore)
+                implementation(Deps.Kotlinx.serializationCore)
+                with(Deps.Test) {
+                    implementation(junit)
+                    implementation(core)
+                    implementation(coroutinesTest)
+                }
+            }
+        }
+
+        val jvmMain by getting
+        val androidMain by getting
+        val nativeMain by creating {
+            dependsOn(commonMain)
+        }
+    }
+}
+
+android {
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    compileSdk = 31
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 31
+    }
+
+    lint {
+        disable += "ComposableModifierFactory"
+        disable += "ModifierFactoryExtensionFunction"
+        disable += "ModifierFactoryReturnType"
+        disable += "ModifierFactoryUnreferencedReceiver"
+    }
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    dokkaSourceSets.configureEach {
+        reportUndocumented.set(false)
+        skipDeprecated.set(true)
+        jdkVersion.set(8)
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(S01)
+    signAllPublications()
+}
+
+addGithubPackagesRepository()
+kmmbridge {
+    githubReleaseArtifacts()
+    githubReleaseVersions()
+    versionPrefix.set("5.0.0-alpha")
+    spm()
+}
+
+koverMerged {
+    enable()
+
+    xmlReport {
+        onCheck.set(true)
+        reportFile.set(layout.projectDirectory.file("kover/coverage.xml"))
+    }
+
+    htmlReport {
+        onCheck.set(true)
+        reportDir.set(layout.projectDirectory.dir("kover/html"))
+    }
+
+    verify {
+        onCheck.set(true)
+    }
+}
---
+++ multicast/src/commonMain/kotlin/org/mobilenativefoundation/store/multicast5/Multicaster.kt
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mobilenativefoundation.store.multicast5
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ClosedSendChannelException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.transform
+
+/**
+ * Like a publish, shares 1 upstream value with multiple downstream receiver.
+ *
+ * This operation still keeps the upstream flow cold such that it is suspended until at least 1
+ * downstream value collects the latest dispatched value OR a new downstream is added while [buffer]
+ * is empty.
+ */
+class Multicaster<T>(
+    /**
+     * The [CoroutineScope] to use for upstream subscription
+     */
+    private val scope: CoroutineScope,
+    /**
+     * The buffer size that is used only if the upstream has not complete yet.
+     * Defaults to 0.
+     */
+    bufferSize: Int = 0,
+    /**
+     * Source function to create a new flow when necessary.
+     */
+    private val source: Flow<T>,
+
+    /**
+     * If true, downstream is never closed by the multicaster unless upstream throws an error.
+     * Instead, it is kept open and if a new downstream shows up that causes us to restart the flow,
+     * it will receive values as well.
+     */
+    private val piggybackingDownstream: Boolean = false,
+    /**
+     * If true, an active upstream will stay alive even if all downstreams are closed. A downstream
+     * coming in later will receive a value from the live upstream.
+     *
+     * The upstream will be kept alive until [scope] cancels or [close] is called.
+     */
+    private val keepUpstreamAlive: Boolean = false,
+    /**
+     * Called when upstream dispatches a value.
+     */
+    private val onEach: suspend (T) -> Unit
+) {
+
+    private val channelManager by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+        ChannelManager(
+            scope = scope,
+            bufferSize = bufferSize,
+            upstream = source,
+            piggybackingDownstream = piggybackingDownstream,
+            keepUpstreamAlive = keepUpstreamAlive,
+            onEach = onEach
+        )
+    }
+
+    /**
+     * Gets a new downstream flow. Collectors of this flow will share values dispatched by a
+     * single upstream [source] Flow.
+     *
+     * @param piggybackOnly if true this downstream will not cause a new upstream to start running
+     * (which only happens if no upstream is running already, e.g if this is the first downstream
+     * added). [piggybackOnly] is only valid if [piggybackingDownstream] is enabled for this
+     * [Multicaster].
+     */
+    fun newDownstream(piggybackOnly: Boolean = false): Flow<T> {
+        check(!piggybackOnly || piggybackingDownstream) {
+            "cannot create a piggyback only flow when piggybackDownstream is disabled"
+        }
+        return flow {
+            val channel = Channel<ChannelManager.Message.Dispatch.Value<T>>(Channel.UNLIMITED)
+            val subFlow = channel.consumeAsFlow()
+                .onStart {
+                    try {
+                        channelManager.addDownstream(channel, piggybackOnly)
+                    } catch (closed: ClosedSendChannelException) {
+                        // before we could start, channel manager was closed.
+                        // close our downstream manually as it won't be closed by the ChannelManager
+                        channel.close()
+                    }
+                }
+                .transform<ChannelManager.Message.Dispatch.Value<T>, T> {
+                    emit(it.value)
+                    it.delivered.complete(Unit)
+                }.onCompletion {
+                    try {
+                        channelManager.removeDownstream(channel)
+                    } catch (closed: ClosedSendChannelException) {
+                        // ignore, we might be closed because ChannelManager is closed
+                    }
+                }
+            emitAll(subFlow)
+        }
+    }
+
+    /**
+     * Closes the [Multicaster]. All current collectors on the [flow] will complete and any new
+     * collector will receive 0 values and immediately close even if the [bufferSize] is set to a
+     * positive value.
+     *
+     * This is an idempotent operation.
+     */
+    suspend fun close() {
+        channelManager.close()
+    }
+}
