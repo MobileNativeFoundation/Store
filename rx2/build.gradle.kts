4a91477a9aa793e99ccdb2dc68f9a67f08ee449c
Matt Ramotar <mramotar@dropbox.com>
2023-03-08 20:34:42 -0500

    Add rx2 module

---
+++ rx2/build.gradle.kts
@@ -0,0 +1,54 @@
+@file:Suppress("UnstableApiUsage")
+
+import com.vanniktech.maven.publish.SonatypeHost.S01
+import org.jetbrains.dokka.gradle.DokkaTask
+
+plugins {
+    kotlin("android")
+    id("com.android.library")
+    id("com.vanniktech.maven.publish")
+    id("org.jetbrains.dokka")
+    id("org.jetbrains.kotlinx.kover")
+    `maven-publish`
+    kotlin("native.cocoapods")
+}
+
+dependencies {
+    implementation(project(":store"))
+}
+
+
+android {
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 33
+    }
+
+    lint {
+        disable += "ComposableModifierFactory"
+        disable += "ModifierFactoryExtensionFunction"
+        disable += "ModifierFactoryReturnType"
+        disable += "ModifierFactoryUnreferencedReceiver"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    dokkaSourceSets.configureEach {
+        reportUndocumented.set(false)
+        skipDeprecated.set(true)
+        jdkVersion.set(8)
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(S01)
+    signAllPublications()
+}
