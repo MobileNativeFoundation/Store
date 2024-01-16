e7ac9635dddb6a19693fe466615506200256dba0
mramotar <mramotar@dropbox.com>
2024-01-15 22:06:43 -0500

    Building your first Store
    Signed-off-by: mramotar <mramotar@dropbox.com>

--- website/src/components/StoreNewsAppImage.tsx
+++ website/src/components/StoreNewsAppImage.tsx
@@ -3,6 +3,6 @@ import storeNewsAppImg from "@site/static/img/store_news_app.png"
 
 export function StoreNewsAppImage() {
     return (
-        <img src={storeNewsAppImg} alt="Store news app" style={{height: 750}}/>
+        <img src={storeNewsAppImg} alt="Store news app" style={{height: 500}}/>
     )
 }
