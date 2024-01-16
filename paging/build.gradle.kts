8558ed07bfdce70b05781b693921d23219bcfe12
mramotar <mramotar@dropbox.com>
2024-01-15 22:22:05 -0500

    Remove molecule
    Signed-off-by: mramotar <mramotar@dropbox.com>

--- paging/build.gradle.kts
+++ paging/build.gradle.kts
@@ -21,7 +21,6 @@ kotlin {
                 implementation(project(":store"))
                 implementation(project(":cache"))
                 implementation(compose.runtime)
-                implementation(libs.molecule.runtime)
                 implementation(compose.ui)
                 implementation(compose.foundation)
                 implementation(compose.material)
