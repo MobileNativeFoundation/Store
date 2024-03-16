26b7baa915d5717cc01a7a4cb2d385a0da23be5c
mramotar <mramotar@dropbox.com>
2024-03-16 10:18:22 -0400

    Add paging:core module
    Signed-off-by: mramotar <mramotar@dropbox.com>

---
+++ paging/core/build.gradle.kts
@@ -0,0 +1,77 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("org.jetbrains.dokka")
+    id("org.jetbrains.kotlinx.kover")
+}
+
+kotlin {
+    androidTarget()
+    jvm()
+    iosArm64()
+    iosX64()
+    linuxX64()
+    iosSimulatorArm64()
+    js {
+        browser()
+        nodejs()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kotlin.stdlib)
+                implementation(libs.touchlab.kermit)
+                implementation(libs.kotlinx.coroutines.core)
+                api(libs.store)
+            }
+        }
+
+        val androidMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(libs.turbine)
+                implementation(libs.kotlinx.coroutines.test)
+            }
+        }
+    }
+
+    jvmToolchain(11)
+}
+
+android {
+    namespace = "org.mobilenativefoundation.paging.core"
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    dokkaSourceSets.configureEach {
+        reportUndocumented.set(false)
+        skipDeprecated.set(true)
+        jdkVersion.set(11)
+    }
+}
+
+koverMerged {
+    enable()
+
+    xmlReport {
+        onCheck.set(true)
+        reportFile.set(layout.projectDirectory.file("kover/coverage.xml"))
+    }
+
+    htmlReport {
+        onCheck.set(true)
+        reportDir.set(layout.projectDirectory.dir("kover/html"))
+    }
+
+    verify {
+        onCheck.set(true)
+    }
+}
