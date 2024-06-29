f7c56538165d2ee44adeab2abc425e8cda036638
Matt Ramotar <matt.ramotar@uber.com>
2024-06-29 12:16:53 -0400

    Add binary compatibility validator and convention plugins (#645)
    Signed-off-by: matt-ramotar <matt.ramotar@uber.com>

--- multicast/build.gradle.kts
+++ multicast/build.gradle.kts
@@ -1,45 +1,10 @@
-@file:Suppress("UnstableApiUsage")
-
-import com.vanniktech.maven.publish.SonatypeHost.S01
-import org.jetbrains.dokka.gradle.DokkaTask
-
 plugins {
-    kotlin("multiplatform")
-    kotlin("plugin.serialization")
-    id("com.android.library")
-    id("com.vanniktech.maven.publish")
-    id("org.jetbrains.dokka")
-    id("org.jetbrains.kotlinx.kover")
-    `maven-publish`
-    kotlin("native.cocoapods")
-    id("kotlinx-atomicfu")
+    id("org.mobilenativefoundation.store.multiplatform")
 }
 
 kotlin {
-    android()
-    jvm()
-    iosArm64()
-    iosX64()
-    linuxX64()
-    iosSimulatorArm64()
-    js {
-        browser()
-        nodejs()
-    }
-    cocoapods {
-        summary = "Multicast5"
-        homepage = "https://github.com/MobileNativeFoundation/Store"
-        ios.deploymentTarget = "13"
-        version = libs.versions.store.get()
-    }
 
     sourceSets {
-        all {
-            languageSettings.apply {
-                optIn("kotlinx.coroutines.ExperimentalCoroutinesApi")
-                optIn("kotlin.RequiresOptIn")
-            }
-        }
 
         val commonMain by getting {
             dependencies {
@@ -50,74 +15,14 @@ kotlin {
 
         val commonTest by getting {
             dependencies {
-                implementation(kotlin("test"))
                 implementation(libs.junit)
                 implementation(libs.kotlinx.coroutines.test)
                 implementation(libs.turbine)
             }
         }
-
-        val jvmMain by getting
-        val androidMain by getting
-        val nativeMain by creating {
-            dependsOn(commonMain)
-        }
     }
-
-    jvmToolchain(17)
 }
 
 android {
-    namespace = "org.mobilenativefoundation.store.multicast5"
-
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 33
-    }
-
-    lint {
-        disable += "ComposableModifierFactory"
-        disable += "ModifierFactoryExtensionFunction"
-        disable += "ModifierFactoryReturnType"
-        disable += "ModifierFactoryUnreferencedReceiver"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
-
-tasks.withType<DokkaTask>().configureEach {
-    dokkaSourceSets.configureEach {
-        reportUndocumented.set(false)
-        skipDeprecated.set(true)
-        jdkVersion.set(17)
-    }
-}
-
-mavenPublishing {
-    publishToMavenCentral(S01)
-    signAllPublications()
-}
-
-koverMerged {
-    enable()
-
-    xmlReport {
-        onCheck.set(true)
-        reportFile.set(layout.projectDirectory.file("kover/coverage.xml"))
-    }
-
-    htmlReport {
-        onCheck.set(true)
-        reportDir.set(layout.projectDirectory.dir("kover/html"))
-    }
-
-    verify {
-        onCheck.set(true)
-    }
+    namespace = "org.mobilenativefoundation.store.multicast"
 }
---
+++ tooling/plugins/src/main/kotlin/org/mobilenativefoundation/store/tooling/plugins/AndroidConventionPlugin.kt
@@ -0,0 +1,54 @@
+@file:Suppress("UnstableApiUsage")
+
+package org.mobilenativefoundation.store.tooling.plugins
+
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidConventionPlugin : Plugin<Project> {
+    override fun apply(project: Project) = with(project) {
+        with(pluginManager) {
+            apply("org.jetbrains.kotlin.android")
+            apply("com.android.library")
+            apply("com.vanniktech.maven.publish")
+            apply("org.jetbrains.dokka")
+            apply("org.jetbrains.kotlinx.kover")
+            apply("maven-publish")
+            apply("org.jetbrains.kotlin.native.cocoapods")
+            apply("org.jetbrains.kotlinx.binary-compatibility-validator")
+        }
+
+
+        extensions.configure<LibraryExtension> {
+
+            compileSdk = 33
+
+            defaultConfig {
+                minSdk = 24
+                targetSdk = 33
+            }
+
+            lint {
+                disable += "ComposableModifierFactory"
+                disable += "ModifierFactoryExtensionFunction"
+                disable += "ModifierFactoryReturnType"
+                disable += "ModifierFactoryUnreferencedReceiver"
+            }
+
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+
+        configureKotlin()
+        configureDokka()
+        configureMavenPublishing()
+    }
+}
+
+
+
