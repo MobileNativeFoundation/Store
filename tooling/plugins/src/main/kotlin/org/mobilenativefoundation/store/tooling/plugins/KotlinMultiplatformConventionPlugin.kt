19aa8f81e978143e947fbf91577db89864f83ebf
Matt Ramotar <matt.ramotar@uber.com>
2024-10-18 10:05:51 -0400

    Release 5.1.0-alpha05 (#664)
    Signed-off-by: matt-ramotar <matt.ramotar@uber.com>

--- tooling/plugins/src/main/kotlin/org/mobilenativefoundation/store/tooling/plugins/KotlinMultiplatformConventionPlugin.kt
+++ tooling/plugins/src/main/kotlin/org/mobilenativefoundation/store/tooling/plugins/KotlinMultiplatformConventionPlugin.kt
@@ -172,7 +172,7 @@ object Versions {
     const val COMPILE_SDK = 34
     const val MIN_SDK = 24
     const val TARGET_SDK = 34
-    const val STORE = "5.1.0-SNAPSHOT"
+    const val STORE = "5.1.0-alpha05"
 }
 
 
