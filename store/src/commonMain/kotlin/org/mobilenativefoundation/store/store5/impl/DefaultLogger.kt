6d24f547563ba0c75bc7b8bdc3af475227135de4
matt-ramotar <matt.ramotar@uber.com>
2024-11-17 17:23:30 -0500

    Add default implementation of Logger
    Signed-off-by: matt-ramotar <matt.ramotar@uber.com>

---
+++ store/src/commonMain/kotlin/org/mobilenativefoundation/store/store5/impl/DefaultLogger.kt
@@ -0,0 +1,24 @@
+package org.mobilenativefoundation.store.store5.impl
+
+import co.touchlab.kermit.CommonWriter
+import org.mobilenativefoundation.store.store5.Logger
+
+/**
+ * Default implementation of [Logger] using the Kermit logging library.
+ */
+class DefaultLogger : Logger {
+
+    private val delegate =
+        co.touchlab.kermit.Logger.apply {
+            setLogWriters(listOf(CommonWriter()))
+            setTag("Store")
+        }
+
+    override fun debug(message: String) {
+        delegate.d(message)
+    }
+
+    override fun error(message: String, throwable: Throwable?) {
+        delegate.e(message, throwable)
+    }
+}
