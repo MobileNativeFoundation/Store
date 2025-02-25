680134430fdea64d80ce0e3366a99c55169c2d45
Matt Ramotar <matt.ramotar@uber.com>
2025-02-25 16:40:07 -0500

    Fix Eager Conflict Resolution Deadlock and Improve Mutable Store Code Quality (#679)
    * Fix and Cover Eager Conflict Resolution Deadlock
    
    Signed-off-by: Matt Ramotar <matt.ramotar@uber.com>
    
    * Cover RealMutableStore
    
    Signed-off-by: Matt Ramotar <matt.ramotar@uber.com>
    
    ---------
    
    Signed-off-by: Matt Ramotar <matt.ramotar@uber.com>

---
+++ store/src/commonTest/kotlin/org/mobilenativefoundation/store/store5/mutablestore/util/TestLogger.kt
@@ -0,0 +1,19 @@
+package org.mobilenativefoundation.store.store5.mutablestore.util
+
+import org.mobilenativefoundation.store.store5.Logger
+
+class TestLogger : Logger {
+    val debugLogs = mutableListOf<String>()
+    val errorLogs = mutableListOf<Pair<String, Throwable?>>()
+
+    override fun debug(message: String) {
+        debugLogs.add(message)
+    }
+
+    override fun error(
+        message: String,
+        throwable: Throwable?,
+    ) {
+        errorLogs.add(message to throwable)
+    }
+}
---
+++ store/src/commonTest/kotlin/org/mobilenativefoundation/store/store5/mutablestore/util/TestSourceOfTruth.kt
@@ -0,0 +1,67 @@
+package org.mobilenativefoundation.store.store5.mutablestore.util
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import org.mobilenativefoundation.store.store5.SourceOfTruth
+
+@Suppress("UNCHECKED_CAST")
+class TestSourceOfTruth<Key : Any, Local : Any, Output : Any> : SourceOfTruth<Key, Local, Output> {
+    private val storage = HashMap<Key, Local?>()
+    private val flows = HashMap<Key, MutableSharedFlow<Output?>>()
+    private var readError: Throwable? = null
+    private var writeError: Throwable? = null
+    private var deleteError: Throwable? = null
+    private var deleteAllError: Throwable? = null
+
+    fun throwOnRead(
+        key: Key,
+        block: () -> Throwable,
+    ) {
+        readError = block()
+    }
+
+    fun throwOnWrite(
+        key: Key,
+        block: () -> Throwable,
+    ) {
+        writeError = block()
+    }
+
+    fun throwOnDelete(
+        key: Key?,
+        block: () -> Throwable,
+    ) {
+        if (key != null) deleteError = block() else deleteAllError = block()
+    }
+
+    override fun reader(key: Key): Flow<Output?> =
+        flow {
+            readError?.let { throw SourceOfTruth.ReadException(key, it) }
+            val sharedFlow = flows.getOrPut(key) { MutableSharedFlow(replay = 1) }
+            emit(storage[key] as Output?)
+            emitAll(sharedFlow)
+        }
+
+    override suspend fun write(
+        key: Key,
+        value: Local,
+    ) {
+        writeError?.let { throw SourceOfTruth.WriteException(key, value, it) }
+        storage[key] = value
+        flows[key]?.emit(value as Output?)
+    }
+
+    override suspend fun delete(key: Key) {
+        deleteError?.let { throw it }
+        storage.remove(key)
+        flows.remove(key)
+    }
+
+    override suspend fun deleteAll() {
+        deleteAllError?.let { throw it }
+        storage.clear()
+        flows.clear()
+    }
+}
