d4e94b41d2a17c9ea8a3a46408ff3e2fd73a65e2
Matt Ramotar <matt.ramotar@uber.com>
2025-09-20 15:37:52 -0400

    Configure Automatic Release to Maven Central
    Signed-off-by: Matt Ramotar <matt.ramotar@uber.com>

--- tooling/plugins/src/main/kotlin/org/mobilenativefoundation/store/tooling/plugins/KotlinMultiplatformConventionPlugin.kt
+++ tooling/plugins/src/main/kotlin/org/mobilenativefoundation/store/tooling/plugins/KotlinMultiplatformConventionPlugin.kt
@@ -175,12 +175,12 @@ object Versions {
     const val COMPILE_SDK = 34
     const val MIN_SDK = 24
     const val TARGET_SDK = 34
-    const val STORE = "5.1.0-alpha07"
+    const val STORE = "5.1.0-SNAPSHOT"
 }
 
 
 fun Project.configureMavenPublishing() = extensions.configure<MavenPublishBaseExtension> {
-    publishToMavenCentral()
+    publishToMavenCentral(automaticRelease = true)
     signAllPublications()
 }
 
